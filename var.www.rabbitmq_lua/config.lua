570bde8fc6b38493ece0126932d602f7be9d2832
linxuping <linxuping@126.com>
2015-08-20 14:01:37 +0800

    pwd ch

--- var.www.rabbitmq_lua/config.lua
+++ var.www.rabbitmq_lua/config.lua
@@ -1,8 +1,8 @@
 local config = {
 	host      = "192.168.12.200",
 	port      = 61613,
-	username  = "teiron",
-	password  = "teiron",
+	username  = "guest",
+	password  = "guest",
 	vhost     = "/",
 	timeout   = 100,
 	error_log = "/var/log/lua_mq/error.log",
